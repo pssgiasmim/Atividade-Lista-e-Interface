745863ee7cd81dbc9c3c8bac4bde7d9915b07a36
pssgiasmim <iasmimpassing@gmail.com>
2024-09-25 11:20:10 -0300

    Mensagens no console de pocao e comida

--- Atividade de Lista e Interface/Assets/Scripts/Pocao.cs
+++ Atividade de Lista e Interface/Assets/Scripts/Pocao.cs
@@ -6,11 +6,11 @@ public class Pocao : Item
 {
     public override void Interagir()
     {
-        
+        Debug.Log("Você pegou a comida");
     }
 
     public override void Consumir()
     {
-
+        Debug.Log("Você comeu a comida");
     }
 }
