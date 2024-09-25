73f64dcb472499ac1f1cc1900369aed34553f461
pssgiasmim <iasmimpassing@gmail.com>
2024-09-25 10:27:28 -0300

    Criação Classe Interface Interagivel (IInteragivel)

---
+++ Atividade de Lista e Interface/Assets/Scripts/IInteragivel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //Método responsávelç pela ação de interagir dos NPCs e dos itens
+    public void Interagir()
+    {
+        
+    }
+}
